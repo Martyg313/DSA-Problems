552b7efdbfbaa4558ebfd5c5c9fec6547e223968
Martin Golec <golec7martin@gmail.com>
2025-07-04 21:33:46 -0400

    leetcode 94.

---
+++ 94. Binary Tree Inorder Traversal/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _94._Binary_Tree_Inorder_Traversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //InorderTraversal(your_input);
+        }
+
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            // List Creation
+            List<int> output = new List<int>();
+
+            return InorderTraversalRecursive(root, output);
+        }
+
+        public IList<int> InorderTraversalRecursive(TreeNode root, List<int> output)
+        {
+            if (root == null)
+            {
+                // Do nothing
+            }
+            else
+            {
+                if (root.left != null)
+                {
+                    InorderTraversalRecursive(root.left, output);
+                }
+
+                output.Add(root.val);
+
+                if (root.right != null)
+                {
+                    InorderTraversalRecursive(root.right, output);
+                }
+            }
+
+            return output;
+        }
+    }
+}

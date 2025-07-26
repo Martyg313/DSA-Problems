42ba6cde5a18d8080dcb7fa001af4fa0a260750b
Martin Golec <golec7martin@gmail.com>
2025-07-26 14:29:40 -0400

    leetcode 83.

---
+++ 83. Remove Duplicates from Sorted List/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _83._Remove_Duplicates_from_Sorted_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DeleteDuplicates(your_input);
+        }
+
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null)
+                return head;
+            else
+            {
+                ListNode start = head;
+                ListNode lastNode = head;
+                head = head.next;
+
+                while (head != null)
+                {
+                    if (head.val == lastNode.val)
+                        lastNode.next = head.next;
+                    else
+                        lastNode = head;
+                    head = head.next;
+                }
+                return start;
+            }
+        }
+    }
+}

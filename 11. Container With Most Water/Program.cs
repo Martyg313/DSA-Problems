4a1cd48cc8e2d7e2540c18cb087b4025ecf57191
Martin Golec <golec7martin@gmail.com>
2025-08-10 18:47:36 -0400

    leetcode 11.

---
+++ 11. Container With Most Water/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _11._Container_With_Most_Water
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MaxArea(your_input);
+        }
+
+        public int MaxArea(int[] height)
+        {
+            int first = 0, second = height.Length - 1, max = 0;
+
+            while (first < second)
+            {
+                int difference = second - first;
+                int area = 0;
+                if (height[first] >= height[second])
+                {
+                    area = height[second] * difference;
+                    second -= 1;
+                }
+                else
+                {
+                    area = height[first] * difference;
+                    first += 1;
+                }
+                if (area > max)
+                    max = area;
+            }
+
+            return max;
+        }
+    }
+}

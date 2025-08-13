329fd7e4086ca4217c728485d0a3bae34d699a56
Martin Golec <golec7martin@gmail.com>
2025-08-13 14:43:07 -0400

    leetcode 144.

---
+++ 144. Binary Tree Preorder Traversal/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _144._Binary_Tree_Preorder_Traversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PreorderTraversal(your_input);
+        }
+
+        List<int> preOrder = new List<int>();
+
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            //check for empty node
+            if (root == null)
+            {
+                return preOrder;
+            }
+
+            // Action -> recurse left -> recurse right
+            preOrder.Add(root.val);
+            if (root.left != null)
+                PreorderTraversal(root.left);
+            if (root.right != null)
+                PreorderTraversal(root.right);
+
+            return preOrder;
+        }
+    }
+}

15341b4239376ee12c87132fed435e5454ab9cd5
Martin Golec <golec7martin@gmail.com>
2025-09-07 19:31:07 -0400

    leetcode 21.

---
+++ 21. Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _21._Merge_Two_Sorted_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MergeTwoLists(your_input);
+        }
+
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode temp = new ListNode(-1);
+            ListNode curr = temp;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    curr.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    curr.next = list2;
+                    list2 = list2.next;
+                }
+                curr = curr.next;
+            }
+            if (list1 != null)
+                curr.next = list1;
+            else
+                curr.next = list2;
+            return temp.next;
+        }
+    }
+}

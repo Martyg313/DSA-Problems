2029cdeddcd52c8b54cde5ec403dcfe4fcb82e1b
Martin Golec <golec7martin@gmail.com>
2025-10-14 23:52:19 -0400

    leetcode 101.

---
+++ 101. Symmetric Tree/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _101._Symmetric_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IsSymmetric(your_input);
+        }
+
+        private bool DFS(TreeNode leftRoot, TreeNode rightRoot)
+        {
+            if (leftRoot == null && rightRoot == null)
+            {
+                return true;
+            }
+            if (leftRoot == null || rightRoot == null)
+            {
+                return false;
+            }
+            if (leftRoot.val != rightRoot.val)
+            {
+                return false;
+            }
+
+            bool isCheckOne = DFS(rightRoot.right, leftRoot.left);
+            bool isCheckTwo = DFS(rightRoot.left, leftRoot.right);
+            return isCheckOne && isCheckTwo ? true : false;
+        }
+        public bool IsSymmetric(TreeNode root)
+        {
+            return DFS(root.left, root.right);
+        }
+    }
+}
